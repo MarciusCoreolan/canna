96c6d4cc918145561d2fd161aa97f278719da92c
FeNRiR <germes955@gamail.com>
2022-02-09 23:31:33 +0300

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import sound from '../../assets/sound on.png'
 import canna from '../../assets/PUSHUP_SHADOW_00000.png'
+import header_title from '../../assets/header_title.png'
+import boombox from '../../assets/BOOMBOX_00000.png'
+import work_in_progress from '../../assets/work_in_progress.png'
 
 function Header(props) {
     return (
         <div className={'header'}>
+            <div className={'header_title'}>
+                <img src={header_title} alt=""/>
+            </div>
             <div className={'header_sound'}>
                 <img src={sound} alt=""/>
             </div>
-            <div className={'header_push_canna'}>
-                <img src={canna} alt=""/>
+            <div className={'work_in_progress'}>
+                <img src={work_in_progress} alt=""/>
             </div>
+
+            <div className={'header_main_container'}>
+                <div className={'boombox'}>
+                    <img src={boombox} alt=""/>
+                </div>
+                <div className={'header_push_canna'}>
+                    <img src={canna} alt=""/>
+                </div>
+            </div>
+
         </div>
     );
 }
