524c3e31b660587acbe81dfccb9eec72d5c0c9eb
FeNRiR <germes955@gamail.com>
2022-02-10 15:41:37 +0300

    fix footer and header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import footer from '../../assets/footer_bg.png'
+import React from "react";
+import footer_canna from "../../assets/footer_canna.png";
+import discord from "../../assets/discord bubble.png";
+import twitter from "../../assets/twitter bubble.png";
+import insta from "../../assets/instagram bubble.png";
+import continued from "../../assets/to be continued bubble.png";
+
 function Footer(props) {
-    return (
-        <div className={'footer'}>
-            <img src={footer} alt=""/>
-        </div>
-    );
+  return (
+    <div className={"footer"}>
+      <div className={"footer_img_container discord"}>
+          <a href={'https://discord.com'}>
+              <img src={discord} alt="" />
+          </a>
+      </div>
+      <div className={"footer_img_container twitter"}>
+          <a href={'https://twitter.com'} >
+              <img src={twitter} alt="" />
+          </a>
+      </div>
+
+      <div className={"footer_container canna_container"}>
+        <img src={footer_canna} alt="" />
+      </div>
+
+      <div className={"footer_img_container insta"}>
+          <a href={'https://www.instagram.com/'} >
+              <img src={insta} alt="" />
+          </a>
+      </div>
+      <div className={"footer_img_container continued"}>
+        <img src={continued} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import sound from '../../assets/sound on.png'
+import React, {useState} from 'react';
+import soundOn from '../../assets/sound on.png'
+import soundOff from '../../assets/sound off.png'
 import canna from '../../assets/PUSHUP_SHADOW_00000.png'
 import header_title from '../../assets/header_title.png'
 import boombox from '../../assets/BOOMBOX_00000.png'
 import work_in_progress from '../../assets/work_in_progress.png'
 
 function Header(props) {
+    const [sound, setSound] = useState(false)
+    const handleClick = () =>{
+        setSound(!sound)
+    }
     return (
         <div className={'header'}>
             <div className={'header_title'}>
                 <img src={header_title} alt=""/>
             </div>
-            <div className={'header_sound'}>
-                <img src={sound} alt=""/>
+            <div className={'header_sound'} onClick={handleClick}>
+                <img src={sound ? soundOn : soundOff} alt=""/>
             </div>
             <div className={'work_in_progress'}>
                 <img src={work_in_progress} alt=""/>
