91dc99e6b0d0002f3ad6fef6c607ad838e94a38e
FeNRiR <germes955@gamail.com>
2022-02-13 10:20:26 +0300

    deploy

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from "./Header/Header";
+import ComixSection from "./Comix/ComixSection";
+import CannaSlider from "./CannaSlider/CannaSlider";
+import Footer from "./Footer/Footer";
+
+function App(props) {
+    return (
+        <div>
+          <Header/>
+          <ComixSection/>
+          <CannaSlider/>
+          <Footer/>
+        </div>
+    );
+}
+
+export default App;
