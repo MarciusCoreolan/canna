d9eb6b0c5a4ffd837b88f9cb957f860e6f0bc0fb
FeNRiR <germes955@gamail.com>
2022-02-19 22:44:37 +0300

    comix

--- src/components/Comix/ComixSection.jsx
+++ src/components/Comix/ComixSection.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import comix from '../../assets/comix/comix_bg.webp'
+import bubble_top from '../../assets/comix/bubble_top.webp'
+import bubble_bottom from '../../assets/comix/bubble_bottom.webp'
+import comix_text from '../../assets/comix/comix_text.webp'
+
 function ComixSection(props) {
     return (
         <div className={'comix_section'}>
             <img src={comix} alt=""/>
+            <div className={'bubble_top'}>
+                <img src={bubble_top} alt=""/>
+            </div>
+            <div className={'bubble_bottom'}>
+                <img src={bubble_bottom} alt=""/>
+            </div>
+            <div className={'comix_text'}>
+                <img src={comix_text} alt=""/>
+            </div>
         </div>
     );
 }
